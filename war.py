797791aa1d9e342d3b314dbb2e76f506da233354
Kizito Naanma <naanma4kizito@gmail.com>
2020-04-20 12:54:20 +0100

    odified

--- war.py
+++ war.py
@@ -1,8 +1,6 @@
-#####################################
-### WELCOME TO YOUR OOP PROJECT #####
-#####################################
 
-# For this project you will be using OOP to create a card game. This card game will
+
+# For this project i will be using OOP to create a card game. This card game will
 # be the card game "War" for two players, you an the computer. If you don't know
 # how to play "War" here are the basic rules:
 #
@@ -20,10 +18,7 @@
 # (six cards). If the turned-up cards are again the same rank, each player places
 # another card face down and turns another card face up. The player with the
 # higher card takes all 10 cards, and so on.
-#
-# There are some more variations on this but we will keep it simple for now.
-# Ignore "double" wars
-#
+
 # https://en.wikipedia.org/wiki/War_(card_game)
 
 from random import shuffle
